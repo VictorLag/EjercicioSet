2cf8569e9217ca6751d51d9445a0a710afa51d67
VictorLag <153596770+VictorLag@users.noreply.github.com>
2024-03-25 13:36:24 +0100

    Delete PlanetaEnano.java

--- PlanetaEnano.java
+++
@@ -1,9 +0,0 @@
-public class PlanetaEnano extends CuerpoCeleste{
-    public PlanetaEnano(String nombre, double periodoOrbital, TipoCuerpoCeleste tipoCuerpo) {
-        super(nombre, periodoOrbital, tipoCuerpo);
-
-        tipoCuerpo=TipoCuerpoCeleste.Planeta_Enano;
-        new CuerpoCeleste(nombre, periodoOrbital, tipoCuerpo);
-
-    }
-}
